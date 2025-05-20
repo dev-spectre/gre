5b1587d7338cb35ba6f29a76c08993bf6f3ed408
dev-spectre <abhishekdallasalpy@gmail.com>
2025-05-20 12:42:49 +0530

    Add hero section animation

---
+++ components/Animations.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+export function HeroAnimation() {
+  return (
+    <DotLottieReact
+      src="/animations/hero-animation.lottie"
+      loop
+      autoplay
+    />
+  );
+}
